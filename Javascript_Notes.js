5db2c023a9fd0d09b9d0c0ec86f61257f81ebf1c
Huseyin <huseyinozdogan@gmail.com>
2021-04-19 16:36:17 +0300

    1.1 commit

---
+++ Javascript_Notes.js
@@ -0,0 +1,15 @@
+primitive types: 
+  Boolean
+  Number
+  String
+  null 
+  undefined 
+NaN  is kind of 0/0 
+Infinity  is kind of 1/0 
+unary operators are operators in one side like counter++ , 2sides are kinf of counter + 3;
+undefined means NOTHING OVER THERE such as favSong[110] , favSong = werewfd iken 
+thing.method() , ex: string.upperCase()
+thing.method(arg1, arg2, ...) thing.indexOf("ball") gibi "superhero".slice(0,5) gibi 
+  "baseball".replace("ba","hent") // hentsball
+string template literals , we use back tick `${}` this will be turned to string 
+
